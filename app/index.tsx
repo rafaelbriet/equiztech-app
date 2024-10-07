bfc8f17fbccb7c01d446af3516fc396a4b6e25f0
Rafael Briet <rafael-briet@hotmail.com>
2024-10-06 22:26:34 -0300

    Commit inicial

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello, Expo World!</Text>
+      <Text>This will become the Equiztech App.</Text>
+    </View>
+  );
+}
