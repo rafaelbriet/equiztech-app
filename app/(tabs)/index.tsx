a8440527279f0f756d1e099bd8570ae7e5142201
Rafael Briet <rafael-briet@hotmail.com>
2024-11-10 23:37:17 -0300

    Add achievements to home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,16 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import React, { useContext, useEffect, useState } from 'react';
 import AppContext from '@/components/AppContext';
 import { Link, router, useNavigation } from "expo-router";
-import { Button, Dialog, Portal, Text } from "react-native-paper";
+import { ActivityIndicator, Avatar, Button, Card, Dialog, Portal, Text } from "react-native-paper";
 import UserProfileContext from "@/components/UserProfileContext";
 
 export default function Index() {
     const {token, setToken} = useContext(AppContext);
     const [logoutDialogAction, setLogoutDialogAction] = useState({} as any);
-    const {userProfile} = useContext(UserProfileContext)
+    const {userProfile, isUserProfileLoaded} = useContext(UserProfileContext)
+    const [achievements, setAchievement] = useState({} as any);
+    const [isFetchingAchievements, setIsFetchingAchievements] = useState(true);
 
     if (token == null) {
         console.log('needs token');
@@ -35,6 +37,53 @@ export default function Index() {
         navigation.dispatch(action)
     }
 
+    async function fetchAchievements() {
+        setIsFetchingAchievements(true);
+
+        try {
+            const params = new URLSearchParams({
+                id_usuario: userProfile.usuario.id
+            });
+            const response = await fetch(process.env.EXPO_PUBLIC_BASE_URL + '/api/conquistas/?' + params.toString(), {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": 'Bearer ' + token.token,
+                },
+            });
+            const data = await response.json();
+            
+            if (data.erro) {
+                console.error(data.erro);
+            } else {
+                setAchievement(data);
+                console.log('achievements fetched');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        console.log('achievements length: ' + Object.keys(achievements).length);
+        
+        if (Object.keys(achievements).length > 0) {
+            setIsFetchingAchievements(false);
+            console.log('achievements loaded:');
+            console.log(achievements);
+        }
+    }, [achievements])
+
+    useEffect(() => {
+        console.log('isUserProfileLoaded:' + isUserProfileLoaded);
+        if (isUserProfileLoaded) {
+            console.log('fetching Achievements');
+            console.log(userProfile);
+            
+            (async () => await fetchAchievements())();
+        }
+    }, [isUserProfileLoaded]);
+
     useEffect(() => {
         navigation.addListener('beforeRemove', (e) => {
             e.preventDefault();
@@ -44,6 +93,20 @@ export default function Index() {
         })
     }, [])
     
+    if (isFetchingAchievements) {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    padding: 16
+                }}
+            >
+                <ActivityIndicator animating={true} />
+            </View>
+        );
+    }
+
     return (
         <View
             style={{
@@ -52,8 +115,46 @@ export default function Index() {
                 padding: 16
             }}
         >
+            <Text variant="titleLarge" style={{ marginBottom: 16 }}>Olá, {userProfile.usuario.nome}</Text>
+            <Text variant="headlineLarge" style={{ marginBottom: 16 }}>Conquistas</Text>
 
-            <Text>Aqui serão exibidas as estatísticas do jogador.</Text>
+            <ScrollView>
+                <Card style={{ marginBottom: 16 }}>
+                    <Card.Title title="Total de partidas" left={(props) => <Avatar.Icon {...props} icon="play" />} />
+                    <Card.Content>
+                        <Text>Você já jogou {achievements.conquistas.total_partidas} quizes.</Text>
+                    </Card.Content>
+                </Card>
+
+                <Card style={{ marginBottom: 16 }}>
+                    <Card.Title title="Total de respostas" left={(props) => <Avatar.Icon {...props} icon="checkbox-multiple-marked" />} />
+                    <Card.Content>
+                        <Text>Você já respondeu {achievements.conquistas.total_respostas} perguntas.</Text>
+                    </Card.Content>
+                </Card>
+
+                <Card style={{ marginBottom: 16 }}>
+                    <Card.Title title="Total de respostas corretas" left={(props) => <Avatar.Icon {...props} icon="star" />} />
+                    <Card.Content>
+                        <Text>Você já respondeu {achievements.conquistas.total_respostas_correta} perguntas corretamente.</Text>
+                    </Card.Content>
+                </Card>
+
+                <Card style={{ marginBottom: 16 }}>
+                    <Card.Title title="Maior quantidade de partidas em um dia" left={(props) => <Avatar.Icon {...props} icon="playlist-play" />} />
+                    <Card.Content>
+                        <Text>Você jogou {achievements.conquistas.maior_quantidade_partida_dia} partidas em um único dia.</Text>
+                    </Card.Content>
+                </Card>
+
+                <Card style={{ marginBottom: 16 }}>
+                    <Card.Title title="Maior sequência de dias jogados" left={(props) => <Avatar.Icon {...props} icon="calendar-multiple-check" />} />
+                    <Card.Content>
+                        <Text>Você jogou {achievements.conquistas.maior_sequencia_dia_jogados} dias seguidos.</Text>
+                    </Card.Content>
+                </Card>
+            </ScrollView>
+
             <Portal>
                 <Dialog visible={visible} onDismiss={hideDialog}>
                     <Dialog.Title>Você deseja sair do app?</Dialog.Title>
